149e0862961bc72b0beb4d64149e646bc7724e40
Tyler Bettilyon <Bettilyont@gmail.com>
2023-02-21 20:11:46 -0600

    forgot to push

--- commandantWebsiteUI/src/app/app.module.ts
+++ commandantWebsiteUI/src/app/app.module.ts
@@ -5,18 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { HeaderComponent } from './component/header/header.component';
+import { BackgroundimageComponent } from './component/backgroundimage/backgroundimage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent
-  ],
+    HeaderComponent,
+    BackgroundimageComponent
+    ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [BackgroundimageComponent]
 })
 export class AppModule { }
